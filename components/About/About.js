e8c997e7a51e66fe21d00ac35e753f6a71f2c328
Gaurav <57193590+gaurav884@users.noreply.github.com>
2022-02-18 11:17:41 +0530

    Update About.js

--- components/About/About.js
+++ components/About/About.js
@@ -31,7 +31,7 @@ const About = () => {
         <div className={AboutStyles.aboutMeDetailsInfoContainer}>
           <h1>Hi, I&apos;m <span>Gaurav!</span></h1>
           <p className={AboutStyles.aboutMeDetailsInfoPara1}>Web Developer</p>
-          <p className={AboutStyles.aboutMeDetailsInfoPara2}>Currently I am a persuing my B.Tech(19-23) in Electrical Engineering at National Institute of Technology Hamirpur. Also I am open to design oportunities. If you want, you can view my <a rel="noopener noreferrer" href="https://www.linkedin.com/in/gaurav-thakur-862129203/" target="_blank" className={AboutStyles.link}>LinkedIn</a> profile, download my updated <a rel="noopener noreferrer" href="https://drive.google.com/drive/folders/1R8OOjZYNrQASJ3L7IMBiMkQsOVbHJhwN" target="_blank" className={AboutStyles.link}>resume</a> or see my latest work down here.</p>
+          <p className={AboutStyles.aboutMeDetailsInfoPara2}>Currently I am a pursuing my B.Tech(19-23) in Electrical Engineering at National Institute of Technology Hamirpur. Also I am open to design oportunities. If you want, you can view my <a rel="noopener noreferrer" href="https://www.linkedin.com/in/gaurav-thakur-862129203/" target="_blank" className={AboutStyles.link}>LinkedIn</a> profile, download my updated <a rel="noopener noreferrer" href="https://drive.google.com/drive/folders/1R8OOjZYNrQASJ3L7IMBiMkQsOVbHJhwN" target="_blank" className={AboutStyles.link}>resume</a> or see my latest work down here.</p>
         </div>
       </div>
     </div>
