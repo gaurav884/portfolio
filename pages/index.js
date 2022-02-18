8e76780865a297a617c01b5f277b964248079f18
Gaurav <57193590+gaurav884@users.noreply.github.com>
2022-02-18 11:16:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Resume</title>
+        <title>Portfolio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet" />
